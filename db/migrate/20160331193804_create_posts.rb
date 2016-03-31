a994f97508043a108b1762eaf49df7d1be259d20
Valeria Zimina <lerarunge@gmail.com>
2016-03-31 22:39:03 +0300

    new Post model

---
+++ db/migrate/20160331193804_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :description
+      t.string :phone
+      t.string :email
+      t.string :address
+      t.string :url
+
+      t.timestamps null: false
+    end
+  end
+end
