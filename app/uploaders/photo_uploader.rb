430963613b08047e304329b12a2fd40103c6def0
Valeria Zimina <lerarunge@gmail.com>
2016-04-14 21:38:52 +0300

    Add styles for card

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -32,9 +32,12 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
+
   version :thumb do
-    process :resize_to_fit => [100, 100]
+    process :resize_to_fit => [10, 10]
   end
+
+
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
